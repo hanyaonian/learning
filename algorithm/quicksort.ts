e4eb308c307eb05c887751d2a803bec67860d8cf
Michael <hanyaonian@outlook.com>
2021-01-30 10:57:15 +0800

    update quicksort's random pick

--- algorithm/quicksort.ts
+++ algorithm/quicksort.ts
@@ -40,35 +40,46 @@ function cmp(pre: number, next: number): number {
 }
 
 /**
- * real sort function
- * @param arr 
- * @param compare 
- * @param start 
- * @param end 
- */
-function sort(arr: any[], compare: compareFunction, left: number, right: number): void {
-	if (left >= right) {
-		return;
-	}
-	let start = left, end = right;
-	// optimize: random pivot
-	// let pivot = getRandom(start, end);
-	let pivot = left;
-	let temp = arr[pivot];
-	while (start < end) {
-		// 右比pivot大
-		while (end > start && compare(arr[end], temp) >= 0) {
-			end -= 1;
-		}
-		arr[start] = arr[end];
-		while (end > start && compare(arr[start], temp) < 0) {
-			start += 1;
-		}
-		arr[end] = arr[start];
-	}
-	// start === end
-	arr[start] = temp;
-	// remember to lower the boundary
-	sort(arr, compare, left, pivot - 1);
-	sort(arr, compare, pivot + 1, right);
+  * swap array elements
+  * @param arr 
+  * @param pos1 
+  * @param pos2 
+  */
+function swap(arr: any[], pos1: number, pos2: number): void {
+	let temp = arr[pos1];
+	arr[pos1] = arr[pos2];
+	arr[pos2] = temp;
 }
+ /**
+  * real sort function
+  * @param arr 
+  * @param compare 
+  * @param start 
+  * @param end 
+  */
+ function sort(arr: any[], compare: compareFunction, left: number, right: number): void {
+	 if (left >= right) {
+		 return;
+	 }
+	 let start = left, end = right;
+	 // optimize: random pivot
+	 let pivot = getRandom(start, end);
+	 let temp = arr[pivot];
+	 swap(arr, pivot, start);
+	 while (start < end) {
+		 // 右比pivot大
+		 while (end > start && compare(arr[end], temp) >= 0) {
+			 end -= 1;
+		 }
+		 arr[start] = arr[end];
+		 while (end > start && compare(arr[start], temp) < 0) {
+			 start += 1;
+		 }
+		 arr[end] = arr[start];
+	 }
+	 // start === end
+	 arr[start] = temp;
+	 // remember to lower the boundary
+	 sort(arr, compare, left, start - 1);
+	 sort(arr, compare, start + 1, right);
+ }
