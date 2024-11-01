ce407440619f5b6a7d61e1ddfb93e2595c474d01
MichaelHyn <hanyaonian@outlook.com>
2024-11-02 00:00:24 +0800

    docs: ts types

---
+++ programming-language/ts-programming/tips/promise-returntype.ts
@@ -0,0 +1,9 @@
+/**
+ * get return type of a promise-based function
+ * 
+ * explain:
+    1. `T extends (...args: any) =>` 限制类型为函数
+    2. `ReturnType<T> extends Promise<infer U> ? U : never;` 如果ReturnType是promise, 那么返回U (分支条件), 否则无效推导;
+ */
+export type ResAwaitedType<T extends (...args: any) => any> =
+  ReturnType<T> extends Promise<infer U> ? U : never;
---
+++ programming-language/ts-programming/tips/use-types-in-js.ts
@@ -0,0 +1,11 @@
+/**
+ * using types in js
+ *
+ * @see https://www.typescriptlang.org/docs/handbook/type-checking-javascript-files.html
+ *
+ * example below
+ */
+
+/**
+ * @type {import("eslint").Linter.Config}
+ */
